0f08d88c147b5ef7873e9e120f92a3519cc08e65
AGlowczewska <ola.glowczewska95@gmail.com>
2018-01-08 18:05:43 +0100

    Fronted for login/signin pages

--- the_guard_app/views.py
+++ the_guard_app/views.py
@@ -4,7 +4,10 @@ from the_guard.pyrebase_settings import db
 
 def index(request):
     context = {}
-    return render(request, 'index.html', context)
+    if request.user.is_authenticated():
+        return render(request, 'index.html', context)
+    else:
+        return render(request, 'registration/login.html', context)
 
 
 def get_users(request):
