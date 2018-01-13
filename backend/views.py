d81ed0dd56674e910765228f59686b3dcb5766f8
AGlowczewska <ola.glowczewska95@gmail.com>
2018-01-13 13:47:14 +0100

    small update

--- backend/views.py
+++ backend/views.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from rest_framework.decorators import api_view
@@ -116,8 +115,3 @@ def sensors_update(request, format=json):
 def delete_rasp(request):
     if request.method == 'POST':
         return
-=======
-`from django.shortcuts import render
-
-# Create your views here.
->>>>>>> 0ab28e39f101ea93b60de9b31bcb651f01454fd7
