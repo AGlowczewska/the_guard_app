2f7d251545f02ebf5918e5ecbe30ff06d8209cae
AGlowczewska <ola.glowczewska95@gmail.com>
2018-01-15 22:39:38 +0100

    Showing names, adding rasp registration, checking if rasp is connected to user before showing it

--- the_guard_app/views.py
+++ the_guard_app/views.py
@@ -1,41 +1,69 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from the_guard.pyrebase_settings import db
+from backend.models import Rasps
+from django.db import IntegrityError
 
 
-def user_rasps():
-    return db.child("sensor").get()
+def user_rasps(username):
+    firebase_result = db.child("sensor").get().val()
+    my_rasps = Rasps.objects.filter(owner=username)
+
+    for firebase_rasp in firebase_result:
+        to_delete = True
+        name = ''
+        for django_rasp in my_rasps:
+            if django_rasp.serial == firebase_rasp:
+                to_delete = False
+                name = django_rasp.name
+        if to_delete:
+            del(firebase_result[firebase_rasp])
+            print('Not in result - deleted: ', firebase_rasp)
+        else:
+            firebase_result[firebase_rasp]['name'] = name
+
+    return firebase_result
 
 
 @login_required
 def rasp_view(request, rasp_serial):
-    rooms = user_rasps()
+    rooms = user_rasps(request.user.username)
 
-    context = {'rooms': rooms.val(), 'name': rasp_serial}
+    context = {'rooms': rooms, 'name': rooms[rasp_serial]['name'], 'serial': rasp_serial}
     return render(request, 'parts/data_area.html', context)
 
 
+@login_required
 def index(request):
     context = {}
     if request.user.is_authenticated():
-        rooms = user_rasps()
-        print(rooms.val())
-        context = {'rooms': rooms.val()}
+        rooms = user_rasps(request.user.username)
+        context = {'rooms': rooms}
         # stream = db.child("sensors").stream(stream_handler)
         return render(request, 'index.html', context)
     else:
         return render(request, 'registration/login.html', context)
 
 
+@login_required
 def connect_rasp(request):
-    rooms = user_rasps()
-    context = {'rooms': rooms.val()}
+    rooms = user_rasps(request.user.username)
+    context = {'rooms': rooms}
 
     if request.method == 'POST':
         serial = request.POST['serial']
+        name = request.POST['name']
         context['serial'] = serial
-
+        username = request.user.username
+        my_rasp = Rasps(owner=username, name=name, serial=serial)
         # adding to db
+        try:
+            my_rasp.save()
+            context['msg'] = 'Successfully added to db'
+        except IntegrityError as err:
+            context['msg'] = 'ERROR: This serial already exists in database!'
+        except Exception as err:
+            context['msg'] = err
         return render(request, 'parts/add_rasp.html', context)
 
     return render(request, 'parts/add_rasp.html', context)
@@ -51,6 +79,3 @@ def stream_handler(message):
     print(context)
     # request = HttpRequest()
     # return render(request, 'index.html', context)
-
-
-
