078a472f4f2b68f3df6c24eb7093fdb9d2e85551
pablo.szudrowicz@gmail.com <pablo.szudrowicz@gmail.com>
2018-01-26 17:12:50 +0000

    Working

--- backend/views.py
+++ backend/views.py
@@ -16,7 +16,6 @@ import cv2
 import time
 import subprocess
 
-
 initialized = False
 
 #kurwa siedzialem nad tym 2h a okazalo sie ze jest blad w fcmnotification  i to nie jest api_key tylko SERVER_KEY !!! JPRDL
